91626dadc48dc95c0e0d8d85d46cca5657fb51a9
Dominic Lourenço Barbosa <mobarbosa.phoenix@gmail.com>
2022-01-03 11:36:01 -0300

    correção do Enum Uf

--- src/desafio2/model/Uf.java
+++ src/desafio2/model/Uf.java
@@ -2,9 +2,32 @@ package desafio2.model;
 
 public enum Uf {
 
-
-	SP,
+	AC,
+	AL,
+	AM,
+	AP,
+	BA,
+	CE,
+	DF,
+	ES,
+	GO,
+	MA,
+	MG,
+	MS,
+	MT,
+	PA,
+	PB,
+	PE,
+	PI,
+	PR,
+	RJ,
+	RN,
+	RO,
+	RR,
+	RS,
+	SC,
 	SE,
+	SP,
 	TO;
 	
 	
